419d56e677749d1bcd1f53c2c3a9641551eeaaf6
Jack1stUNL <jmcgarvey3@huskers.unl.edu>
2020-08-22 20:06:01 -0700

    Initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: John McGarvey
+ *         jmcgarvey3@huskers.unl.edu
+ * Date: 2020/08/18
+ *
+ * Hello world program our first lab
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("John McGarvey, but I go by Jack!\n");
+  printf("My major is Computer Science!\n");
+  
+  return 0;
+}
