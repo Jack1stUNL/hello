3015daf894381774215fddeadadad489cf69885f
Jack1stUNL <jmcgarvey3@huskers.unl.edu>
2020-08-22 20:40:07 -0700

    2 new printf functions
    Added 2 new printf functions to print my name and major to satisfy the needs of the coding assignment.

---
+++ hello master/Hello.-master/hello.c
@@ -0,0 +1,20 @@
+/**
+*Diego Almanza Zavala
+*2020/08/17
+*
+* A simple hello world program in C
+*
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main( int argc, char **argv) {
+
+
+printf("Diego Almanza!\n");
+printf("Electrical Engineer\n");
+printf("John McGarvey, but I go by Jack!\n");
+printf("My major is Computer Science!\n");
+
+return 0;
+}
